2602904a318fd83d401f5a1ac24b2a9652158313
espressom <espressom@naver.com>
2022-07-11 17:22:17 +0900

    test: Spring Data JPA 테스트 및 JUnit 테스트 완료

--- backend/src/test/java/com/example/backend/domain/posts/PostsRepositoryTest.java
+++ backend/src/test/java/com/example/backend/domain/posts/PostsRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -17,10 +18,10 @@ public class PostsRepositoryTest {
     @Autowired
     PostsRepository postsRepository;
 
-    @After
-    public void cleanup() {
-        postsRepository.deleteAll();
-    }
+//    @After
+//    public void cleanup() {
+//        postsRepository.deleteAll();
+//    }
 
     @Test
     public void 게시글저장_불러오기() {
@@ -38,5 +39,33 @@ public class PostsRepositoryTest {
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
     }
+
+    @Test
+    public void 게시글_내림차순_불러오기() {
+        List<Posts> allPosts = postsRepository.findAllDesc();
+        for(Posts posts : allPosts) {
+            System.out.println(posts.getCreatedDate());
+        }
+    }
+
+    @Test
+    public void BaseTimeEntity_등록() {
+        // given
+        LocalDateTime now = LocalDateTime.of(2019, 6, 4, 0, 0, 0);
+        postsRepository.save(Posts.builder().title("제목").content("내용").author("작성자").build());
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(postsList.size()-1);
+
+        System.out.println(">>>> createDate="+posts.getCreatedDate()+", modifiedDate="+posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+    }
+
+
 
 }
--- backend/src/test/java/com/example/backend/web/PostsApiControllerTest.java
+++ backend/src/test/java/com/example/backend/web/PostsApiControllerTest.java
@@ -1,15 +1,26 @@
 package com.example.backend.web;
 
+import com.example.backend.domain.posts.Posts;
 import com.example.backend.domain.posts.PostsRepository;
+import com.example.backend.web.dto.PostsResponseDto;
 import com.example.backend.web.dto.PostsSaveRequestDto;
+import com.example.backend.web.dto.PostsUpdateRequestDto;
 import org.junit.After;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -24,11 +35,12 @@ public class PostsApiControllerTest {
     @Autowired
     private PostsRepository postsRepository;
 
-    @After
+//    @After
     public void tearDown() throws Exception {
         postsRepository.deleteAll();
     }
 
+    @Test
     public void Posts_등록된다() throws Exception {
         // given
         String title = "title";
@@ -41,6 +53,62 @@ public class PostsApiControllerTest {
         ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
 
     }
+
+    @Test
+    public void Posts_조회된다() throws Exception {
+        // given
+        Posts registeredPost = postsRepository.save(Posts.builder().title("테스트 제목").author("김코딩").content("테스트 내용").build());
+        String url = "http://localhost:"+port+"/api/v1/posts/"+registeredPost.getId();
+
+        // when
+        ResponseEntity<Posts> resp = restTemplate.getForEntity(url, Posts.class);
+        System.out.println(resp);
+
+        // then
+        assertThat(resp.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(resp.getBody().getAuthor()).isEqualTo("김코딩");
+
+    }
+
+    @Test
+    public void Posts_수정된다() throws Exception {
+        // given
+
+        // 수정을 확인하기 위해 먼저 게시글을 저장
+        String title = "title";
+        String content = "content";
+        Posts savedPosts = postsRepository.save(Posts.builder().title(title).content(content).author("author").build());
+        // 저장한 객체를 DAO(postsRepository)에서 가져오고, id 값에 접근
+        Long updateId = savedPosts.getId();
+        System.out.println(updateId);
+
+        // 갱신 시 사용할 DTO 생성
+        String expectedTitle = "title2";
+        String expectedContent = "content2";
+        PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder().title(expectedTitle).content(expectedContent).build();
+
+
+        String url = "http://localhost:"+port+"/api/v1/posts/"+updateId;
+
+        // 별도의 헤더는 추가하지 않고 메시지만 담아 HttpEntity 객체 생성
+        HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
+
+        // when
+        // 브라우저 대신 restTemplate을 사용해 요청을 보내고 응답 결과를 responseEntity에 저장
+        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+
+        // then
+        // responseEntity의 응답 상태코드가 200인지 확인
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+//        assertThat(requestEntity.getBody()).isGreaterThan(0L); // 오류
+
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(all.size()-1).getTitle()).isEqualTo(expectedTitle);
+        assertThat(all.get(all.size()-1).getContent()).isEqualTo(expectedContent);
+
+
+    }
+
 
 
 }
