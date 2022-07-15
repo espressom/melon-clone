e648fc2b36b9d6a40ce33c31a7853fdf7709a2ca
espressom <espressom@naver.com>
2022-07-15 17:26:09 +0900

    feat: User Playlist 도메인 추가

--- backend/src/main/java/com/example/backend/domain/playlists/Playlist.java
+++ backend/src/main/java/com/example/backend/domain/playlists/Playlist.java
@@ -1,5 +1,8 @@
 package com.example.backend.domain.playlists;
 
+import com.example.backend.domain.BaseTime;
+import com.example.backend.domain.songs.Song;
+import com.example.backend.domain.users_playlists.UserPlaylist;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -7,8 +10,17 @@ import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor
-//@Entity
+@Entity(name = "playlists")
 public class Playlist {
 
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "playlist_id")
+    private UserPlaylist userPlaylist;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "song_id")
+    private Song song;
+
+    private int song_like_status;
 
 }
